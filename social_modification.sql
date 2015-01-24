01f1937bd814d7a2dd6edac3f3cd6aaec7e28276
helenxuyu <xiaoyuerxuyu@gmail.com>
2015-01-23 19:43:19 -0600

    social modification solution

---
+++ social_modification.sql
@@ -0,0 +1,86 @@
+/* Create the database and use the database */
+use social;
+
+/* Delete the tables if they already exist */
+drop table if exists Highschooler;
+drop table if exists Friend;
+drop table if exists Likes;
+
+/* Create the schema for our tables */
+create table Highschooler(ID int, name text, grade int);
+create table Friend(ID1 int, ID2 int);
+create table Likes(ID1 int, ID2 int);
+
+/* Populate the tables with our data */
+insert into Highschooler values (1510, 'Jordan', 9);
+insert into Highschooler values (1689, 'Gabriel', 9);
+insert into Highschooler values (1381, 'Tiffany', 9);
+insert into Highschooler values (1709, 'Cassandra', 9);
+insert into Highschooler values (1101, 'Haley', 10);
+insert into Highschooler values (1782, 'Andrew', 10);
+insert into Highschooler values (1468, 'Kris', 10);
+insert into Highschooler values (1641, 'Brittany', 10);
+insert into Highschooler values (1247, 'Alexis', 11);
+insert into Highschooler values (1316, 'Austin', 11);
+insert into Highschooler values (1911, 'Gabriel', 11);
+insert into Highschooler values (1501, 'Jessica', 11);
+insert into Highschooler values (1304, 'Jordan', 12);
+insert into Highschooler values (1025, 'John', 12);
+insert into Highschooler values (1934, 'Kyle', 12);
+insert into Highschooler values (1661, 'Logan', 12);
+
+insert into Friend values (1510, 1381);
+insert into Friend values (1510, 1689);
+insert into Friend values (1689, 1709);
+insert into Friend values (1381, 1247);
+insert into Friend values (1709, 1247);
+insert into Friend values (1689, 1782);
+insert into Friend values (1782, 1468);
+insert into Friend values (1782, 1316);
+insert into Friend values (1782, 1304);
+insert into Friend values (1468, 1101);
+insert into Friend values (1468, 1641);
+insert into Friend values (1101, 1641);
+insert into Friend values (1247, 1911);
+insert into Friend values (1247, 1501);
+insert into Friend values (1911, 1501);
+insert into Friend values (1501, 1934);
+insert into Friend values (1316, 1934);
+insert into Friend values (1934, 1304);
+insert into Friend values (1304, 1661);
+insert into Friend values (1661, 1025);
+insert into Friend select ID2, ID1 from Friend;
+
+insert into Likes values(1689, 1709);
+insert into Likes values(1709, 1689);
+insert into Likes values(1782, 1709);
+insert into Likes values(1911, 1247);
+insert into Likes values(1247, 1468);
+insert into Likes values(1641, 1468);
+insert into Likes values(1316, 1304);
+insert into Likes values(1501, 1934);
+insert into Likes values(1934, 1501);
+insert into Likes values(1025, 1101);
+
+/*It's time for the seniors to graduate. Remove all 12th graders from Highschooler.*/
+delete from Highschooler
+where grade = 12;
+
+/*If two students A and B are friends, and A likes B but not vice-versa, remove the Likes tuple.*/
+delete from Likes
+where ID2 in (select ID2 from Friend where Likes.ID1 = ID1) and
+ID2 not in (select L.ID1 f Likes L where Likes.ID1 = L.ID2);
+
+
+/*For all cases where A is friends with B, and B is friends with C, add a new friendship for the pair A and C. Do not add duplicate friendships, friendships that already exist, or friendships with oneself.
+(This one is a bit challenging; congratulations if you get it right.)*/
+
+SET SQL_SAFE_UPDATES = 0;
+
+insert into Friend
+select F1.ID1, F2.ID2
+from Friend as F1, Friend as F2
+where F1.ID2 = F2.ID1 and F1.ID1 <> F2.ID2
+and (F1.ID1, F2.ID2) not in (select * from Friend);
+
+
